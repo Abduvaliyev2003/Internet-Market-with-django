c27e8625502f826483025786791a1d5e3f10720c
Abduvaliyev2003 <abduvaliyevasad23@gmail.com>
2026-06-07 22:47:31 +0500

    django file structure

--- app1/main/views.py
+++ app1/main/views.py
@@ -1,3 +1,15 @@
+from http.client import HTTPResponse
+from multiprocessing import context
+
+from django.db.models.fields import return_None
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request) -> HttpResponse:
+    context: [str, str] = {
+        'title': 'Home',
+        'content': 'Главная странница магазина - Home'
+    }
+    return render(request, 'main/index.html', context)
