600a936a26d851f32cf85da8c111ebf49b2d57de
Abduvaliyev2003 <abduvaliyevasad23@gmail.com>
2026-06-12 00:42:07 +0500

    added search function

--- app1/goods/templatetags/goods_tags.py
+++ app1/goods/templatetags/goods_tags.py
@@ -1,4 +1,8 @@
+
+
 from django import template
+from django.utils.http import urlencode
+
 from goods.models import Categories
 
 
@@ -6,3 +10,10 @@ register = template.Library()
 
 def tag_catergories():
     return Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+
+    return urlencode(query)
--- app1/goods/views.py
+++ app1/goods/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 from goods.models import Products
+from goods.utils import q_search
 
 
 # Create your views here.
@@ -13,20 +14,22 @@ def catalog(request, category_slug=None, page=1):
 
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
-
+    query = request.GET.get('q', None)
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = Products.objects.filter(
             category__slug=category_slug
         )
     if on_sale:
-        goods = goods.filter(discount_gt=0)
+        goods = goods.filter(discount__gt=0)
 
     if order_by and order_by != 'default':
         goods = goods.order_by(order_by)
 
-    paginator = Paginator(goods, 3)
+    paginator = Paginator(goods, 2)
     page = request.GET.get('page', 1)
     current_page = paginator.get_page(page)
 
