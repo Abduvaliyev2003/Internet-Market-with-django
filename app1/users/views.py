e769a8a1d05fabc771441dbfc7c8a1145f4e97fc
Abduvaliyev2003 <abduvaliyevasad23@gmail.com>
2026-06-17 11:23:03 +0500

    fix users urls

---
+++ app1/users/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def login(request) -> HttpResponse:
+    context: dict[str, str] = {
+        'title': 'Home - Авторизация'
+    }
+
+    return render(request, 'users/login.html', context)
+
+
+def registration(request) -> HttpResponse:
+    context: dict[str, str] = {
+        'title': 'Home - Регистрация'
+    }
+
+    return render(request, 'users/registration.html', context)
+
+
+def profile(request) -> HttpResponse:
+    context: dict[str, str] = {
+        'title': 'Home - Кабинет'
+    }
+
+    return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    pass
